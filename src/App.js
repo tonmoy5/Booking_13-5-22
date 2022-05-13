0fbbb080e2ebbe3e8c833d027b1756df1736204c
tonmoy5 <tonmoy52532@gmail.com>
2022-05-13 14:17:29 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Routes
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<List />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
         </Routes>
       </Router>
     </div>
