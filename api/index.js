a13f4f91496201976f4a4cf8f07fb4674f98350f
tonmoy5 <tonmoy52532@gmail.com>
2022-05-16 15:26:47 +0600

    adding cookie

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import usersRoute from "./routes/users.js";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ mongoose.connection.on('disconnected', () => {
 });
 
 //middlewares
+app.use(cookieParser())
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
