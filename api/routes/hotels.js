b8109b582359c9ea1dc3163046be28ce68bf2344
tonmoy5 <tonmoy52532@gmail.com>
2022-05-16 18:40:28 +0600

    frontend done

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,23 +1,33 @@
-import express from 'express';
-import { creteHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from '../controllers/hotel.js';
-import { verifyAdmin } from '../utils/verifyToken.js';
-
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js"
 const router = express.Router();
 
-
-//CREAKTE
-router.post("/", verifyAdmin, creteHotel);
+//CREATE
+router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
-
 //DELETE
-router.delete("/:id", verifyAdmin, deleteHotel)
-
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/:id", getHotel)
 
+router.get("/find/:id", getHotel);
 //GET ALL
-router.get("/", getAllHotel)
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
